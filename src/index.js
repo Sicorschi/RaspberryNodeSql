1a64b713a9faea0f839fee0503f0c5d9295e83e1
Igor Sicorschi <igor.sicorschi@sngular.com>
2021-01-31 18:58:08 +0100

    init package to node server 2

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello from node server 2!')
+})
+
+app.listen(port, () => {
+  console.log(`Node app 2 listening at http://localhost:${port}`)
+})
